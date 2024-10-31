c66a5df4c15141c9629063e67d179941e9e55e57
shankkarpal46 <shankkarpal46@gmail.com>
2024-10-31 11:01:32 +0530

    adding caritems

--- superpet/cart/views.py
+++ superpet/cart/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render,HttpResponseRedirect
-
+from .models import Cart,CartItem,Product
+#from products.models import Product
 # Create your views here.
 
 def add_to_cart(request,productId):
     print(productId)
     print(request.user)
     currentUser = request.user
-    return HttpResponseRedirect("/products")
+    cart_obj,created = Cart.objects.get_or_create(user = currentUser)
+    cartitem,created = CartItem.objects.get_or_create(cart = cart_obj,products = Product.customManager.get(id = productId))
+    quantity = int(request.GET.get("quantity")) #type cast in string 
+
+    if created:
+        cartitem.quantity = quantity
+    else:
+        cartitem.quantity += quantity 
+    
+    cartitem.save() #we are connected with the object
+    return HttpResponseRedirect("/products")    
+    
 
 def display_cart(request):
-    return render(request,"cart.html")
+    currentUser = request.user
+    cart = Cart.objects.get(user=currentUser)
+    cartitems = cart.cartitem_set.all()
+    total = 0
+    for cartitem in cartitems:
+        total += cartitem.quantity * cartitem.products.product_price
+    return render(request,"cart.html",{"cartitems": cartitems,"total":total})
 
